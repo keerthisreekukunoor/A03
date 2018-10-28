e5f42eead0aa2042654710c497737627bb9fb25e
keerthi sree <keerthisree9595@gmail.com>
2018-10-28 18:58:57 -0500

    c

--- public/js/game.js
+++ public/js/game.js
@@ -1,17 +1,18 @@
-var numField1 = document.querySelector('#field1');
-var numField2 = document.querySelector('#field2');
+
 var resultField = document.querySelector('#result');
-var form = document.querySelector('#Percentage').addEventListener('submit', calculatePerentage());
+var form = document.querySelector('#Percentage').addEventListener('submit', calculatePerentage(number1, number2));
 
 function calculatePerentage()
  {
-  if (!field1.value || !field2.value) 
+    var numField1 = parseInt(number1)
+    var numField2 = parseInt(number2)
+  if (!numField1.value || !numField2.value) 
   {
     alert("Please enter values in the fields");
   } 
   else {
-    var val1 = parseFloat(field1.value);
-    var val2 = parseFloat(field2.value);
+    var val1 = parseFloat(numField1.value);
+    var val2 = parseFloat(numField2.value);
 
     var res = val1 / val2;
     var percent = res * 100;
