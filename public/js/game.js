94bc55635c7e438d66987c09896b7a8600afab69
keerthi sree <keerthisree9595@gmail.com>
2018-10-27 21:57:05 -0500

    commit

--- public/js/game.js
+++ public/js/game.js
@@ -2,14 +2,16 @@ var numField1 = document.querySelector('#numField1');
 var numField2 = document.querySelector('#numField2');
 var resultField = document.querySelector('#resultField');
 var form = document.querySelector('#Percentage').addEventListener('submit', function (event) {
-  if (!numField1.value || !numField2.value) {
+  if (!field1.value || !field2.value) 
+  {
     alert("Please enter values in the fields");
-  } else {
-    var x = parseFloat(numField1.value);
-    var y = parseFloat(numField2.value);
+  } 
+  else {
+    var val1 = parseFloat(field1.value);
+    var val2 = parseFloat(field2.value);
 
-    var result = x / y;
-    var percent = result * 100;
+    var res = val1 / val2;
+    var percent = res * 100;
 
     resultField.innerText = "Answer: " + percent + "%";
     event.preventDefault();
