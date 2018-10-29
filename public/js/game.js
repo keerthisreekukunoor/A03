5d22482bbf4a8bed38840721343740115fa61ba4
keerthi sree <keerthisree9595@gmail.com>
2018-10-28 20:15:42 -0500

    c

--- public/js/game.js
+++ public/js/game.js
@@ -1,22 +1,17 @@
+var numField1 = document.querySelector('#numField1');
+var numField2 = document.querySelector('#numField2');
+var resultField = document.querySelector('#resultField');
+var form = document.querySelector('#xIsWhatPercentOfY').addEventListener('submit', function (event) {
+  if (!numField1.value || !numField2.value) {
+    alert("Please enter values in the fields");
+  } else {
+    var x = parseFloat(numField1.value);
+    var y = parseFloat(numField2.value);
 
-var resultField = document.querySelector('#result');
-var form = document.querySelector('#Percentage').addEventListener('submit', calculatePerentage(number1, number2));
+    var result = x / y;
+    var percent = result * 100;
 
-function calculatePerentage(number1, number2) {
-    var numField1 = parseInt(number1)
-    var numField2 = parseInt(number2)
-    if (!numField1.value || !numField2.value) {
-        alert("Please enter values in the fields");
-    }
-    else {
-        var val1 = parseFloat(numField1.value);
-        var val2 = parseFloat(numField2.value);
-
-        var res = val1 / val2;
-        var percent = res * 100;
-
-        resultField.innerText = "Answer: " + percent + "%";
-        event.preventDefault();
-    }
-    return percent;
-}
+    resultField.innerText = "Answer: " + percent + "%";
+    event.preventDefault();
+  }
+});
