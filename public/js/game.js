cf63568e049c8b872288e47c055571363bd450dc
keerthi sree <keerthisree9595@gmail.com>
2018-10-28 22:02:40 -0500

    commit

--- public/js/game.js
+++ public/js/game.js
@@ -1,17 +1,35 @@
-var numField1 = document.querySelector('#numField1');
-var numField2 = document.querySelector('#numField2');
+$("#value1").click(function(){
+var numField1 = $("#numField1").value ?  parseFloat(numField1.value) : numField1
+var numField2 =  $("#numField2").value ?  parseFloat(numField2.value) : numField2
 var resultField = document.querySelector('#resultField');
-var form = document.querySelector('#xIsWhatPercentOfY').addEventListener('submit', function (event) {
-  if (!numField1.value || !numField2.value) {
-    alert("Please enter values in the fields");
-  } else {
-    var x = parseFloat(numField1.value);
-    var y = parseFloat(numField2.value);
+//percalculator(numField1, numField2);
+// var form = document.querySelector('#xIsWhatPercentOfY').addEventListener('submit', function (event)
+// {
 
-    var result = x / y;
-    var percent = result * 100;
-
-    resultField.innerText = "Answer: " + percent + "%";
-    event.preventDefault();
-  }
+percalculator(numField1,numField2)
 });
+
+function percalculator(numField1, numField2)
+{
+    var percent = 0
+    // if (!numField1.value || !numField2.value) {
+    //     alert("Please enter values in the fields");
+    // }
+    // else {
+        //var x = parseFloat(numField1.value);
+        //var y = parseFloat(numField2.value);
+
+        var result = numField1 / numField2;
+        percent = result * 100;
+        console.log(percent)
+        if($("#resultField"))
+        {
+        //resultField.innerText = "Answer: " + percent + "%";
+        $("#resultField").html(percent)
+        //event.preventDefault();
+        }
+    // }
+    console.log(percent)
+    return percent
+}
+// });
